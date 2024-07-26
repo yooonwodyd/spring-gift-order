7af483f65559f3b089382e4ce83a6db11b09bbe1
yooonwodyd <dbswodyd00@naver.com>
2024-07-26 17:09:25 +0900

    [feat] 메서드 위치 변경 및 Lazy 설정 추가

--- src/main/java/gift/product/domain/Option.java
+++ src/main/java/gift/product/domain/Option.java
@@ -1,7 +1,8 @@
-package gift.feat.product.domain;
+package gift.product.domain;
 
 import gift.core.exception.product.NotFoundMoreStockException;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,29 +17,17 @@ public class Option {
 	private String name;
 	private Long quantity;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "product_id")
 	private Product product;
 
 	protected Option() {
 	}
 
-	public Option(String name, Long quantity, Product product) {
+	public Option(String name, Long quantity,Product product) {
 		this.name = name;
 		this.quantity = quantity;
 		this.product = product;
-	}
-
-	public void addStock(int quantity) {
-		this.quantity += quantity;
-	}
-
-	public void removeStock(int quantity) {
-		Long restStock = this.quantity - quantity;
-		if (restStock < 0) {
-			throw new NotFoundMoreStockException();
-		}
-		this.quantity = restStock;
 	}
 
 	public Long getId() {
@@ -65,7 +54,19 @@ public class Option {
 		this.product = product;
 	}
 
-	public static Option of(String name, Long quantity, Product product) {
-		return new Option(name, quantity, product);
+	public static Option create(String name, Long quantity,Product product) {
+		return new Option(name, quantity,product);
+	}
+
+	public void addStock(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeStock(Long quantity) {
+		Long restStock = this.quantity - quantity;
+		if (restStock < 0) {
+			throw new NotFoundMoreStockException();
+		}
+		this.quantity = restStock;
 	}
 }
